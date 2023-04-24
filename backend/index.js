98c4908e02b3418c9b0d2dc917ff9c6e81a3f1a1
jackson <jaxliam1921@gmail.com>
2023-04-24 13:15:26 -0500

    db now functions

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,47 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-2'});
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+
 // Routes
 app.post('/signup', (req, res) => {
-  // Your sign-up logic here
+  // Your sign-up logic herei
   console.log(req.body);
+  console.log("first name = " + req.body.firstName)
 
+  var firstName = req.body.firstName
+  var lastName = req.body.lastName
+  var email = req.body.email;
+  var cellPhone = req.body.cellPhone;
+  var hours = req.body.hours; 
+  var address = req.body.address;
+
+  var params = {
+    TableName: 'mapleLeafs',
+    Item: {
+      'username' : {S: email},
+      'firstName' : {S: firstName},
+      'lastName': {S:lastName},
+      'cellPhone' : {S: cellPhone},
+      'hours':{N:hours},
+      'address' :{S:address},
+      'password':{S:"Null"}
+    }
+  };
+
+// Call DynamoDB to add the item to the table
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+
+  
   // For now, just return a successful response
   res.status(200).json({ message: 'User signed up successfully' });
 });
