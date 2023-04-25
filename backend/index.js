040bfe17c27d8f91eae7cf51c71a9a8e81e84713
jackson <jaxliam1921@gmail.com>
2023-04-24 19:52:43 -0500

    Account storage is now up and running

--- backend/index.js
+++ backend/index.js
@@ -15,16 +15,20 @@ var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 // Routes
 app.post('/signup', (req, res) => {
-  // Your sign-up logic herei
-  console.log(req.body);
-  console.log("first name = " + req.body.firstName)
+  // For now, just return a successful response
+  res.status(200).json({ message: 'User signed up successfully' });
+});
 
+// New route for creating a user with a password
+app.post('/create-user', (req, res) => {
+  
   var firstName = req.body.firstName
   var lastName = req.body.lastName
   var email = req.body.email;
   var cellPhone = req.body.cellPhone;
   var hours = req.body.hours; 
   var address = req.body.address;
+  var password = req.body.password;
 
   var params = {
     TableName: 'mapleLeafs',
@@ -35,11 +39,11 @@ app.post('/signup', (req, res) => {
       'cellPhone' : {S: cellPhone},
       'hours':{N:hours},
       'address' :{S:address},
-      'password':{S:"Null"}
+      'password':{S:password}
     }
   };
 
-// Call DynamoDB to add the item to the table
+  // Call DynamoDB to add the item to the table
   ddb.putItem(params, function(err, data) {
     if (err) {
       console.log("Error", err);
@@ -47,20 +51,6 @@ app.post('/signup', (req, res) => {
       console.log("Success", data);
     }
   });
-
-  
-  // For now, just return a successful response
-  res.status(200).json({ message: 'User signed up successfully' });
-});
-
-// New route for creating a user with a password
-app.post('/create-user', (req, res) => {
-  const userData = req.body;
-
-  console.log('User Data:', userData);
-
-  // Add your user creation logic here
-  // For example, you can save the user to a database, send a confirmation email, etc.
 
   res.status(201).json({ message: 'User created successfully!' });
 });
